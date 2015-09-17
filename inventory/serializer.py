962bdc72052d0b4b2ae8e125fea2767cc2b16bb9
awemulya <aweamulya@gmail.com>
2015-09-17 17:12:10 +0545

    category and item serialization

--- inventory/serializer.py
+++ inventory/serializer.py
@@ -4,22 +4,21 @@ from inventory.models import Category, Item
 
 
 class CategorySerializer(ModelSerializer):
+    parent_name = serializers.ReadOnlyField(source='parent.name')
+
     class Meta:
         model = Category
-        fields = ('name', 'description', 'parent', 'children')
-
-
-CategorySerializer.base_fields['parent'] = CategorySerializer()
-CategorySerializer.base_fields['children'] = CategorySerializer(many=True)
+        fields = ('id', 'name', 'description', 'parent', 'parent_name')
 
 
 class ItemSerializer(serializers.ModelSerializer):
     account_no = serializers.ReadOnlyField(source='account.account_no')
     category_id = serializers.PrimaryKeyRelatedField(source='category', queryset=Category.objects.all())
+    category_name = serializers.ReadOnlyField(source='category.name')
 
     class Meta:
         model = Item
-        fields = ('id', 'code', 'name', 'description', 'category', 'type', 'unit', 'account_no')
+        fields = ('id', 'code', 'name', 'description', 'category_name', 'type', 'unit', 'account_no', 'category_id')
 
 
 class InventoryAccountSerializer(serializers.ModelSerializer):
